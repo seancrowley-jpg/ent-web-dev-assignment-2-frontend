1ea63508b1b72564fdb59da8f0de1ed3f42f373b
Sean-Crowley <seancrowley033@gmail.com>
2021-05-16 18:21:00 +0100

    Pois displayed on page through api call

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,15 @@
 <script>
-</script>
+    import {onMount} from "svelte"
+    import PoiList from "./PoiList.svelte"
 
-<h1> POI Client Application </h1>
+    const url = `http://localhost:4000/api/pois`
+    let poiList;
+    onMount(async () => {
+        const response = await fetch(url)
+        poiList = await response.json();
+    })
+</script>
+<div class="uk-container">
+    <h1> POI Client Application </h1>
+    <PoiList poiList="{poiList}"/>
+</div>
---
+++ src/PoiList.svelte
@@ -0,0 +1,30 @@
+<script>
+    export let poiList;
+</script>
+
+<div class="uk-margin uk-width-2xlarge uk-margin-auto uk-card uk-card-default uk-card-body uk-box-shadow-large">
+    <table class="uk-table">
+        <caption>
+            Walks
+        </caption>
+        <thead>
+        <th>Name</th>
+        <th>Description</th>
+        <th>Added By</th>
+        <th>Category</th>
+        <th>View</th>
+        </thead>
+        <tbody class="uk-text-left">
+        {#if poiList}
+            {#each poiList as poi}
+                <tr>
+                    <td>{poi.name}</td>
+                    <td>{poi.description}</td>
+                    <td></td>
+                    <td>{poi.category}</td>
+                </tr>
+            {/each}
+        {/if}
+        </tbody>
+    </table>
+</div>
