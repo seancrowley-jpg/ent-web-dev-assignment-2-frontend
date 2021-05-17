c23b13ca64e2513c2d1f50c4074d0fc5698bd1cd
Sean-Crowley <seancrowley033@gmail.com>
2021-05-17 12:11:22 +0100

    Added Login page / route

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
     import {setContext} from "svelte";
     import {PoiService} from "./services/poi-service";
     import Main from "./pages/Main.svelte"
+    import Login from "./pages/Login.svelte"
     import Report from "./pages/Report.svelte"
     import Navigator from "./components/Navigator.svelte";
     import Router from "svelte-spa-router";
@@ -11,6 +12,7 @@
 
     let routes = {
         "/" : Main,
+        "/login": Login,
         "/report": Report,
     }
 </script>
