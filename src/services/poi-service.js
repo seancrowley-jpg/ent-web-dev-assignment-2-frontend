7c26a0b4b5e1349fcd009f128d4993ed305d8985
Sean-Crowley <seancrowley033@gmail.com>
2021-05-17 16:12:34 +0100

    Added signup function to poi-service

--- src/services/poi-service.js
+++ src/services/poi-service.js
@@ -65,4 +65,23 @@ export class PoiService {
         });
         this.poiList = [];
     }
+
+    async signup (firstName, lastName, email, password) {
+        try{
+            const userDetails = {
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                password: password,
+            };
+            const response = await axios.post(this.baseUrl + "/api/users", userDetails);
+            const newUser = await response.data;
+            user.set(newUser);
+            return true;
+        } catch (error) {
+            return false;
+        }
+    }
+
+
 }
