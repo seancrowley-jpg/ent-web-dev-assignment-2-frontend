2634e5ba420c7d46f048448e4558b4461379cf99
Sean-Crowley <seancrowley033@gmail.com>
2021-05-17 16:52:28 +0100

    Added Settings page

---
+++ src/pages/Settings.svelte
@@ -0,0 +1,24 @@
+<script>
+    import {mainBar, navBar, title, subTitle} from "../stores";
+    import poiImg from "/src/assets/poi.png"
+    import SettingsForm from "../components/SettingsForm.svelte"
+
+    title.set("Point of Interest Web App");
+    subTitle.set("Your details..")
+
+    navBar.set({
+        bar: mainBar
+    })
+</script>
+
+<div class="uk-flex-middle uk-text-center" uk-grid>
+    <div class="uk-width-1-2@m ">
+        <img width="300" src="{poiImg}" alt="homer">
+    </div>
+    <div class="uk-width-1-2@m">
+        <div class="uk-card uk-card-default uk-card-body uk-box-shadow-large uk-width-2xlarge">
+            <h3 class="uk-card-title uk-text-center">Your Account Details:</h3>
+            <SettingsForm/>
+        </div>
+    </div>
+</div>
